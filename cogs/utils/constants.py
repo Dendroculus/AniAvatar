501b607d099739d59b0e3a025d713dd2ee5e2546
Hans Valerie <hansvalerie19@gmail.com>
2025-12-20 22:33:02 +0700

    Refactor constants and add environment variable loading
    Reorganized ROOT_PATH and COG_PATH assignments for clarity and added loading of environment variables such as DISCORD_TOKEN, GOOGLE_API, and database URLs. This improves configuration management by centralizing sensitive data in environment variables.

--- cogs/utils/constants.py
+++ cogs/utils/constants.py
@@ -1,12 +1,26 @@
 import os
 
-COG_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # points to ...\AniAvatar\cogs
-ROOT_PATH = os.path.dirname(COG_PATH)                                   # points to ...\AniAvatar
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+COG_PATH = os.path.join(ROOT_PATH, "cogs")  
 
 FONT_DIR = os.path.join(ROOT_PATH, "assets", "fonts")
 EMOJI_PATH = os.path.join(ROOT_PATH, "assets", "RANK ICONS")
 BG_PATH = os.path.join(ROOT_PATH, "assets", "background")  # singular
 
+<<<<<<< Updated upstream:cogs/utils/constants.py
+=======
+
+"""
+Load necessary environment variables
+"""
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+GOOGLE_API = os.getenv("GOOGLE_API_KEY")
+GOOGLE_SEARCH_ENGINE = os.getenv("SEARCH_ENGINE_ID")
+DATABASE = os.getenv("DATABASE_URL")
+REDIS_CACHING = os.getenv("REDIS_URL")
+
+>>>>>>> Stashed changes:utils/constants.py
 FONTS = {
     "bold": os.path.join(FONT_DIR, "gg sans Bold.ttf"),
     "medium": os.path.join(FONT_DIR, "gg sans Medium.ttf"),
@@ -27,7 +41,7 @@ TITLE_EMOJI_FILES = {
     "Immortal": os.path.join(EMOJI_PATH, "IMMORTAL.png"),
     "Celestial": os.path.join(EMOJI_PATH, "CELESTIAL.png"),
     "Transcendent": os.path.join(EMOJI_PATH, "TRANSCENDENT.png"),
-    "Aetherborn": os.path.join(EMOJI_PATH, "AETHERBORN.png"),  
+    "Aetherborn": os.path.join(EMOJI_PATH, "AETHERBORN.png"),
     "Cosmic": os.path.join(EMOJI_PATH, "COSMIC.png"),
     "Divine": os.path.join(EMOJI_PATH, "DIVINE.png"),
     "Eternal": os.path.join(EMOJI_PATH, "ETERNAL.png"),
